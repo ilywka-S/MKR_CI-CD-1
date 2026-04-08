6cbc37c3d87490c508a0eb3147cb7e2fc2b8bc9a
ilywka_ <illasalnikov6@gmail.com>
2026-04-08 15:34:57 +0300

    add: load-tasks-function

--- taskmanager.py
+++ taskmanager.py
@@ -1,5 +1,17 @@
+import json
+
 class TaskManager:
     def __init__(self, filename="tasks.txt"):
         self.filename=filename,
         self.tasks = [],
         self.next_id = 1
+
+    def load_tasks(self):
+        with open(self.filename, 'r') as file:
+            data = json.load(file)
+            self.tasks = data
+
+            if self.tasks:
+                for id in self.tasks:
+                    self.next_id=max(id, self.next_id)+1
+
