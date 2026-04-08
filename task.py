04bf89ba61aefa3f829b8dcf60145d035a189e8e
ilywka_ <illasalnikov6@gmail.com>
2026-04-08 15:38:12 +0300

    add: create-dict-class

--- task.py
+++ task.py
@@ -5,4 +5,10 @@ class Task:
         self.date = date,
         self.prority = priority
 
-    
+    def create_dictionary(self):
+        return {
+            "id": self.task_id,
+            "desc": self.description,
+            "date": self.date,
+            "prior": self.prority
+        }
